6996b86256e4e47e822cbe58fdf1bd6072165f40
Keithlara <47436044+Keithlara@users.noreply.github.com>
2024-02-08 23:40:04 +0800

    Update

---
+++ connectdb.php
@@ -0,0 +1,20 @@
+<?php
+
+try{
+
+    $pdo = new PDO('mysql:host=localhost;dbname=pos_barcode_db','root','');
+
+}catch(PDOException $e){
+
+echo $e->getMessage();
+
+}
+
+
+
+//echo'connection success';
+
+
+
+
+?>
